b45c4a9ffd6c4a169743a0015f73062144702bb1
Yael Sasson <154256021+yaelsas@users.noreply.github.com>
2024-01-31 13:24:40 +0200

    first commit of SpellChecker

--- SpellChecker.java
+++ SpellChecker.java
@@ -8,14 +8,32 @@ public class SpellChecker {
 		String[] dictionary = readDictionary("dictionary.txt");
 		String correction = spellChecker(word, threshold, dictionary);
 		System.out.println(correction);
+		
 	}
 
 	public static String tail(String str) {
-		// Your code goes here
+		if (str.length() == 1) {
+			return "";
+		}
+		return str.substring(1);
 	}
 
 	public static int levenshtein(String word1, String word2) {
-		// Your code goes here
+		// number of changes when 1 word is an empty string
+		if (word2.length() == 0) {
+			return word1.length();
+		} else if (word1.length() == 0) {
+			return word2.length();
+		} 
+		// ensures both words are lower case
+		word1 = word1.toLowerCase();
+		word2 = word2.toLowerCase();
+		if (word1.substring(0, 1).equals(word2.substring(0, 1))) {
+			return levenshtein(tail(word1), tail(word2));
+		}
+		// checks minimum changes between deletion insertion and replacement
+		int minChanges = Math.min(levenshtein(tail(word1), word2), levenshtein(word1, tail(word2)));
+		return 1 + Math.min(minChanges, levenshtein(tail(word1), tail(word2)));
 	}
 
 	public static String[] readDictionary(String fileName) {
@@ -23,13 +41,27 @@ public class SpellChecker {
 
 		In in = new In(fileName);
 
-		// Your code here
+		for (int i = 0; i < dictionary.length && !in.isEmpty(); i++) {
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static String spellChecker(String word, int threshold, String[] dictionary) {
-		// Your code goes here
+		int minEditDistance = levenshtein(word, dictionary[0]);
+		int mostSimilarIndex = 0;
+		for (int i = 1; i < dictionary.length; i++) {
+			if (minEditDistance > levenshtein(word, dictionary[i])) {
+				minEditDistance = levenshtein(word, dictionary[i]);
+				mostSimilarIndex = i;
+			}
+		}
+		if (minEditDistance > threshold) {
+			return word;
+		} else {
+			return dictionary[mostSimilarIndex];
+		}
 	}
 
 }
